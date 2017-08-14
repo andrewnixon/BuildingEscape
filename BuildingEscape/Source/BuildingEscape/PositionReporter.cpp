c72d51583f17819636411bd699371ede23f592ec
andrewnixon0 <andrewnixon0@gmail.com>
2017-08-14 22:17:13 +0100

    Position reporter actually does something!

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -9,6 +9,7 @@ UPositionReporter::UPositionReporter()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
@@ -21,7 +22,7 @@ void UPositionReporter::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Position report is working on chair."));
 }
 
 
